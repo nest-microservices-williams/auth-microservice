77d271998a40ba8cf9caf1959d17d7d479b630fe
williamsmata <williams.rm99@gmail.com>
2024-09-22 10:13:02 -0400

    register user done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { LoginUserDto, RegisterUserDto } from './dto';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { CustomRpcException } from 'src/common/exceptions/rpc.exception';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  registerUser(registerUserDto: RegisterUserDto) {
-    return registerUserDto;
+  async registerUser(registerUserDto: RegisterUserDto) {
+    const { name, email, password } = registerUserDto;
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { email },
+      });
+
+      if (user) {
+        throw new Error('User already exists');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = await this.prismaService.user.create({
+        data: { name, email, password: hashedPassword },
+        select: { id: true, name: true, email: true },
+      });
+
+      return {
+        user: newUser,
+      };
+    } catch (error) {
+      throw new CustomRpcException({
+        statusCode: HttpStatus.BAD_REQUEST,
+        error: 'Bad Request',
+        message: error.message,
+      });
+    }
   }
 
   loginUser(loginUserDto: LoginUserDto) {
